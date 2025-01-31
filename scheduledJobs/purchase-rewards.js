8b2a87a981e8a9a0a50adcd8b7938def1077ce36
migueltrevinom <miguel@verifik.co>
2025-01-31 07:12:49 -0600

    add purchase reward schedule job file

---
+++ scheduledJobs/purchase-rewards.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+
+const axios = require("../Core/axios").getCleanInstance();
+
+const token = process.env.SUPER_ADMIN_TOKEN;
+
+const baseUrl = `http://localhost:${process.env.PORT}`;
+
+const _run = async () => {
+	const response = await axios.post(
+		`${baseUrl}/v2/zelf-name-service/purchase-rewards`,
+		{},
+		{
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		}
+	);
+
+	return response.data.data;
+};
+
+_run().then((response) => {
+	console.info({
+		response,
+	});
+});
