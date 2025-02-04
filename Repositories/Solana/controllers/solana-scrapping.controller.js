1c69818b36fabb5f46ccb4add6bf60a5343632e4
anuarmaria <anuarmaria@gmail.com>
2025-02-04 15:52:37 -0500

    fix-data-formatting-solana

--- Repositories/Solana/controllers/solana-scrapping.controller.js
+++ Repositories/Solana/controllers/solana-scrapping.controller.js
@@ -34,7 +34,10 @@ const getToken = async (ctx) => {
 
 const transactionsList = async (ctx) => {
 	try {
-		const data = await Module.getTransactionsList(ctx.request.params, ctx.request.query);
+		const data = await Module.getTransactionsList(
+			ctx.request.params,
+			ctx.request.query
+		);
 
 		ctx.body = { data };
 	} catch (error) {
@@ -46,9 +49,36 @@ const transactionsList = async (ctx) => {
 	}
 };
 
-const transactionStatus = async (ctx) => {
+const transaction = async (ctx) => {
 	try {
-		const data = await Module.getTransactionStatus(ctx.request.params);
+		const data = await Module.getTransaction(ctx.request.params);
+
+		ctx.body = { data };
+	} catch (error) {
+		console.error(error);
+
+		ctx.status = error.status || 500;
+
+		ctx.body = { error: error.message };
+	}
+};
+
+const transfers = async (ctx) => {
+	try {
+		const data = await Module.getTransfers(ctx.request.params);
+
+		ctx.body = { data };
+	} catch (error) {
+		console.error(error);
+
+		ctx.status = error.status || 500;
+
+		ctx.body = { error: error.message };
+	}
+};
+const transfer = async (ctx) => {
+	try {
+		const data = await Module.getTransfer(ctx.request.params);
 
 		ctx.body = { data };
 	} catch (error) {
@@ -62,6 +92,8 @@ const transactionStatus = async (ctx) => {
 module.exports = {
 	getAddress,
 	getToken,
-	transactionStatus,
 	transactionsList,
+	transaction,
+	transfers,
+	transfer,
 };
--- Repositories/Solana/modules/solana-scrapping.module.js
+++ Repositories/Solana/modules/solana-scrapping.module.js
@@ -1,6 +1,8 @@
+const { number } = require("joi");
 const { getCleanInstance } = require("../../../Core/axios");
 const instance = getCleanInstance(30000);
 const { generateRandomUserAgent } = require("../../../Core/helpers");
+const { solana } = require("../../../Core/config");
 const endpoint = `https://api-v2.solscan.io/v2`;
 
 /**
@@ -9,65 +11,75 @@ const endpoint = `https://api-v2.solscan.io/v2`;
 
 const getAddress = async (params) => {
 	try {
-		const { data } = await instance.get(`${endpoint}/account?address=${params.id}`, {
-			headers: {
-				cookie: "__cf_bm=6Nb_BhzZtOdGxVrRrnJoN83pN6rfOtlvuRxjI90nJ0E-1728040658-1.0.1.1-wWYmBJNnH55RvNIFlxo41XFAhHH4kt68pEvyR2fW2Zb_6hVNARhlKxBPCqoZQ_ZhtRQjCeFp6vReppgZbEYgbw; cf_clearance=ibbR6Iox6IDDq4r5WTmuKO0tkKPcwv8GloL.CCp7V3w-1728040665-1.2.1.1-Ef84Taqzk5aSmCiHV321WYtxUv7EIDJTG0SDgp4J.kjIs6gG9J7GVpSV9HE12tuH7xN5vtzdNt1sm6dOee1c20DQO2Lqd.rymeTv.0g340.1jEjK1BnkO4QUlkmEWdbBHzBOc43akGgAef7InJnNEKcg2eO5dPLXT0waBuDCHTs1VMJWBHhfeAE9jZz4C.pPKJKtUDvCyqvR.KRlokRlHo_gnzTVUQDawAtgdLimOOWAK5huvgGQURDoHngS1E5ne03ScjMqfL9HGKME7wXjsK1M9v6zX0WjGMiRDiXkMX3H8oZieb0wyG.j.UKR7jS3K9ElZYtMaZ2kTH1dRJ2DW13.4Q.H3Q.RZlc7Bv3a9FEKf79.2TUxXaiGtKqFH9IbCrPVxFCQ09YxgSp_U0H0AA; _ga=GA1.1.1401977830.1728040664; _ga_PS3V7B7KV0=GS1.1.1728040664.1.1.1728040689.0.0.0; __cf_bm=Mz6cAvTNjPAeUi30bGhjWOHzNal5QLqwS2T6omWu3c8-1728267695-1.0.1.1-oPl.fRzgy_UIvX86I4hXorbEOT5HKcOEAxcTsfbsYnUgtBpIH.Cck88Gm0KcosZyt6sRto_BNTjyJegI2oYryQ",
-				"if-none-match": 'W/"622-Ukdydv8vKaxhKj3QjdlU5A1PS9k"',
-				origin: "https://solscan.io",
-				priority: "u=1, i",
-				referer: "https://solscan.io/",
-				"sol-aut": "5GvLD-NW7B9dls0fKbJHWeJeCUXOPQAbf70dKwfI:",
-				"user-agent": generateRandomUserAgent(),
-			},
-		});
+		const { data } = await instance.get(
+			`${endpoint}/account?address=${params.id}`,
+			{
+				headers: {
+					cookie:
+						"__cf_bm=6Nb_BhzZtOdGxVrRrnJoN83pN6rfOtlvuRxjI90nJ0E-1728040658-1.0.1.1-wWYmBJNnH55RvNIFlxo41XFAhHH4kt68pEvyR2fW2Zb_6hVNARhlKxBPCqoZQ_ZhtRQjCeFp6vReppgZbEYgbw; cf_clearance=ibbR6Iox6IDDq4r5WTmuKO0tkKPcwv8GloL.CCp7V3w-1728040665-1.2.1.1-Ef84Taqzk5aSmCiHV321WYtxUv7EIDJTG0SDgp4J.kjIs6gG9J7GVpSV9HE12tuH7xN5vtzdNt1sm6dOee1c20DQO2Lqd.rymeTv.0g340.1jEjK1BnkO4QUlkmEWdbBHzBOc43akGgAef7InJnNEKcg2eO5dPLXT0waBuDCHTs1VMJWBHhfeAE9jZz4C.pPKJKtUDvCyqvR.KRlokRlHo_gnzTVUQDawAtgdLimOOWAK5huvgGQURDoHngS1E5ne03ScjMqfL9HGKME7wXjsK1M9v6zX0WjGMiRDiXkMX3H8oZieb0wyG.j.UKR7jS3K9ElZYtMaZ2kTH1dRJ2DW13.4Q.H3Q.RZlc7Bv3a9FEKf79.2TUxXaiGtKqFH9IbCrPVxFCQ09YxgSp_U0H0AA; _ga=GA1.1.1401977830.1728040664; _ga_PS3V7B7KV0=GS1.1.1728040664.1.1.1728040689.0.0.0; __cf_bm=Mz6cAvTNjPAeUi30bGhjWOHzNal5QLqwS2T6omWu3c8-1728267695-1.0.1.1-oPl.fRzgy_UIvX86I4hXorbEOT5HKcOEAxcTsfbsYnUgtBpIH.Cck88Gm0KcosZyt6sRto_BNTjyJegI2oYryQ",
+					"if-none-match": 'W/"622-Ukdydv8vKaxhKj3QjdlU5A1PS9k"',
+					origin: "https://solscan.io",
+					priority: "u=1, i",
+					referer: "https://solscan.io/",
+					"sol-aut": "5GvLD-NW7B9dls0fKbJHWeJeCUXOPQAbf70dKwfI:",
+					"user-agent": generateRandomUserAgent(),
+				},
+			}
+		);
 
-		const price_usdt = await instance.get(`${endpoint}/account/domain?address=${params.id}`, {
-			headers: {
-				cookie: "__cf_bm=6Nb_BhzZtOdGxVrRrnJoN83pN6rfOtlvuRxjI90nJ0E-1728040658-1.0.1.1-wWYmBJNnH55RvNIFlxo41XFAhHH4kt68pEvyR2fW2Zb_6hVNARhlKxBPCqoZQ_ZhtRQjCeFp6vReppgZbEYgbw; cf_clearance=ibbR6Iox6IDDq4r5WTmuKO0tkKPcwv8GloL.CCp7V3w-1728040665-1.2.1.1-Ef84Taqzk5aSmCiHV321WYtxUv7EIDJTG0SDgp4J.kjIs6gG9J7GVpSV9HE12tuH7xN5vtzdNt1sm6dOee1c20DQO2Lqd.rymeTv.0g340.1jEjK1BnkO4QUlkmEWdbBHzBOc43akGgAef7InJnNEKcg2eO5dPLXT0waBuDCHTs1VMJWBHhfeAE9jZz4C.pPKJKtUDvCyqvR.KRlokRlHo_gnzTVUQDawAtgdLimOOWAK5huvgGQURDoHngS1E5ne03ScjMqfL9HGKME7wXjsK1M9v6zX0WjGMiRDiXkMX3H8oZieb0wyG.j.UKR7jS3K9ElZYtMaZ2kTH1dRJ2DW13.4Q.H3Q.RZlc7Bv3a9FEKf79.2TUxXaiGtKqFH9IbCrPVxFCQ09YxgSp_U0H0AA; _ga=GA1.1.1401977830.1728040664; _ga_PS3V7B7KV0=GS1.1.1728040664.1.1.1728040689.0.0.0; __cf_bm=Mz6cAvTNjPAeUi30bGhjWOHzNal5QLqwS2T6omWu3c8-1728267695-1.0.1.1-oPl.fRzgy_UIvX86I4hXorbEOT5HKcOEAxcTsfbsYnUgtBpIH.Cck88Gm0KcosZyt6sRto_BNTjyJegI2oYryQ",
-				"if-none-match": 'W/"622-Ukdydv8vKaxhKj3QjdlU5A1PS9k"',
-				origin: "https://solscan.io",
-				priority: "u=1, i",
-				referer: "https://solscan.io/",
-				"sol-aut": "5GvLD-NW7B9dls0fKbJHWeJeCUXOPQAbf70dKwfI:",
-				"user-agent": generateRandomUserAgent(),
-			},
-		});
+		const price_usdt = await instance.get(
+			`${endpoint}/account/domain?address=${params.id}`,
+			{
+				headers: {
+					cookie:
+						"__cf_bm=6Nb_BhzZtOdGxVrRrnJoN83pN6rfOtlvuRxjI90nJ0E-1728040658-1.0.1.1-wWYmBJNnH55RvNIFlxo41XFAhHH4kt68pEvyR2fW2Zb_6hVNARhlKxBPCqoZQ_ZhtRQjCeFp6vReppgZbEYgbw; cf_clearance=ibbR6Iox6IDDq4r5WTmuKO0tkKPcwv8GloL.CCp7V3w-1728040665-1.2.1.1-Ef84Taqzk5aSmCiHV321WYtxUv7EIDJTG0SDgp4J.kjIs6gG9J7GVpSV9HE12tuH7xN5vtzdNt1sm6dOee1c20DQO2Lqd.rymeTv.0g340.1jEjK1BnkO4QUlkmEWdbBHzBOc43akGgAef7InJnNEKcg2eO5dPLXT0waBuDCHTs1VMJWBHhfeAE9jZz4C.pPKJKtUDvCyqvR.KRlokRlHo_gnzTVUQDawAtgdLimOOWAK5huvgGQURDoHngS1E5ne03ScjMqfL9HGKME7wXjsK1M9v6zX0WjGMiRDiXkMX3H8oZieb0wyG.j.UKR7jS3K9ElZYtMaZ2kTH1dRJ2DW13.4Q.H3Q.RZlc7Bv3a9FEKf79.2TUxXaiGtKqFH9IbCrPVxFCQ09YxgSp_U0H0AA; _ga=GA1.1.1401977830.1728040664; _ga_PS3V7B7KV0=GS1.1.1728040664.1.1.1728040689.0.0.0; __cf_bm=Mz6cAvTNjPAeUi30bGhjWOHzNal5QLqwS2T6omWu3c8-1728267695-1.0.1.1-oPl.fRzgy_UIvX86I4hXorbEOT5HKcOEAxcTsfbsYnUgtBpIH.Cck88Gm0KcosZyt6sRto_BNTjyJegI2oYryQ",
+					"if-none-match": 'W/"622-Ukdydv8vKaxhKj3QjdlU5A1PS9k"',
+					origin: "https://solscan.io",
+					priority: "u=1, i",
+					referer: "https://solscan.io/",
+					"sol-aut": "5GvLD-NW7B9dls0fKbJHWeJeCUXOPQAbf70dKwfI:",
+					"user-agent": generateRandomUserAgent(),
+				},
+			}
+		);
 
 		const result = data.data;
-
+		const { transactions } = await getTransactionsList(
+			{ id: params.id },
+			{
+				show: "10",
+			}
+		);
 		const _response = {
 			address: result.account,
 			balance: result.lamports / 1_000_000_000 || 0,
 			type: result.type,
 			account: {
 				asset: "SOL",
-				fiatBalance: "0",
-				price: price_usdt.data.metadata.tokens.So11111111111111111111111111111111111111112.price_usdt,
+				fiatValue: "0",
+				price:
+					price_usdt.data.metadata.tokens
+						.So11111111111111111111111111111111111111112.price_usdt,
 			},
 			tokenHoldings: null,
+
+			solanaTransactions: transactions,
 		};
 
 		if (_response) {
-			_response.fiatBalance = (_response.balance * _response.account.price).toFixed(5);
+			_response.fiatBalance = (
+				_response.balance * _response.account.price
+			).toFixed(5);
 
-			_response.account.fiatBalance = (_response.balance * _response.account.price).toFixed(5);
+			_response.account.fiatValue = (
+				_response.balance * _response.account.price
+			).toFixed(5);
 		}
 
 		_response.tokenHoldings = await getTokens({ id: params.id });
 
-		_response.tokenHoldings.tokens.unshift({
-			tokenType: "SOL",
-			fiatBalance: Number(_response.account.fiatBalance),
-			symbol: "SOL",
-			name: "Solana",
-			price: _response.account.price,
-			image: "https://vtxz26svcpnbg5ncfansdb5zt33ec2bwco6uuah3g3sow3pewfma.arweave.net/rO-delUT2hN1oigbIYe5nvZBaDYTvUoA-zbk623ksVg",
-			amount: _response.balance,
-		});
-
-		_response.transactions = await getTransactionsList({ id: params.id }, { show: 20 });
-
-		if (_response.tokenHoldings.balance) _response.fiatBalance += _response.tokenHoldings.balance;
+		if (_response.tokenHoldings.balance)
+			_response.fiatBalance += _response.tokenHoldings.balance;
 
 		return _response;
 	} catch (error) {
@@ -81,18 +93,43 @@ const getAddress = async (params) => {
  * @returns
  */
 const getTransactionsList = async (params, query) => {
-	const { data } = await instance.get(`${endpoint}/account/transaction?address=${params.id}&limit=${query.show}`, {
-		headers: {
-			cookie: "__cf_bm=6Nb_BhzZtOdGxVrRrnJoN83pN6rfOtlvuRxjI90nJ0E-1728040658-1.0.1.1-wWYmBJNnH55RvNIFlxo41XFAhHH4kt68pEvyR2fW2Zb_6hVNARhlKxBPCqoZQ_ZhtRQjCeFp6vReppgZbEYgbw; cf_clearance=ibbR6Iox6IDDq4r5WTmuKO0tkKPcwv8GloL.CCp7V3w-1728040665-1.2.1.1-Ef84Taqzk5aSmCiHV321WYtxUv7EIDJTG0SDgp4J.kjIs6gG9J7GVpSV9HE12tuH7xN5vtzdNt1sm6dOee1c20DQO2Lqd.rymeTv.0g340.1jEjK1BnkO4QUlkmEWdbBHzBOc43akGgAef7InJnNEKcg2eO5dPLXT0waBuDCHTs1VMJWBHhfeAE9jZz4C.pPKJKtUDvCyqvR.KRlokRlHo_gnzTVUQDawAtgdLimOOWAK5huvgGQURDoHngS1E5ne03ScjMqfL9HGKME7wXjsK1M9v6zX0WjGMiRDiXkMX3H8oZieb0wyG.j.UKR7jS3K9ElZYtMaZ2kTH1dRJ2DW13.4Q.H3Q.RZlc7Bv3a9FEKf79.2TUxXaiGtKqFH9IbCrPVxFCQ09YxgSp_U0H0AA; _ga=GA1.1.1401977830.1728040664; _ga_PS3V7B7KV0=GS1.1.1728040664.1.1.1728040689.0.0.0; __cf_bm=Mz6cAvTNjPAeUi30bGhjWOHzNal5QLqwS2T6omWu3c8-1728267695-1.0.1.1-oPl.fRzgy_UIvX86I4hXorbEOT5HKcOEAxcTsfbsYnUgtBpIH.Cck88Gm0KcosZyt6sRto_BNTjyJegI2oYryQ",
-			"if-none-match": 'W/"622-Ukdydv8vKaxhKj3QjdlU5A1PS9k"',
-			origin: "https://solscan.io",
-			priority: "u=1, i",
-			referer: "https://solscan.io/",
-			"sol-aut": "5GvLD-NW7B9dls0fKbJHWeJeCUXOPQAbf70dKwfI:",
-			"user-agent": generateRandomUserAgent(),
-		},
-	});
-	return { transactions: data.data.transactions };
+	const { data } = await instance.get(
+		`${endpoint}/account/transaction?address=${params.id}&limit=${query.show}`,
+		{
+			headers: {
+				cookie:
+					"__cf_bm=6Nb_BhzZtOdGxVrRrnJoN83pN6rfOtlvuRxjI90nJ0E-1728040658-1.0.1.1-wWYmBJNnH55RvNIFlxo41XFAhHH4kt68pEvyR2fW2Zb_6hVNARhlKxBPCqoZQ_ZhtRQjCeFp6vReppgZbEYgbw; cf_clearance=ibbR6Iox6IDDq4r5WTmuKO0tkKPcwv8GloL.CCp7V3w-1728040665-1.2.1.1-Ef84Taqzk5aSmCiHV321WYtxUv7EIDJTG0SDgp4J.kjIs6gG9J7GVpSV9HE12tuH7xN5vtzdNt1sm6dOee1c20DQO2Lqd.rymeTv.0g340.1jEjK1BnkO4QUlkmEWdbBHzBOc43akGgAef7InJnNEKcg2eO5dPLXT0waBuDCHTs1VMJWBHhfeAE9jZz4C.pPKJKtUDvCyqvR.KRlokRlHo_gnzTVUQDawAtgdLimOOWAK5huvgGQURDoHngS1E5ne03ScjMqfL9HGKME7wXjsK1M9v6zX0WjGMiRDiXkMX3H8oZieb0wyG.j.UKR7jS3K9ElZYtMaZ2kTH1dRJ2DW13.4Q.H3Q.RZlc7Bv3a9FEKf79.2TUxXaiGtKqFH9IbCrPVxFCQ09YxgSp_U0H0AA; _ga=GA1.1.1401977830.1728040664; _ga_PS3V7B7KV0=GS1.1.1728040664.1.1.1728040689.0.0.0; __cf_bm=Mz6cAvTNjPAeUi30bGhjWOHzNal5QLqwS2T6omWu3c8-1728267695-1.0.1.1-oPl.fRzgy_UIvX86I4hXorbEOT5HKcOEAxcTsfbsYnUgtBpIH.Cck88Gm0KcosZyt6sRto_BNTjyJegI2oYryQ",
+				"if-none-match": 'W/"622-Ukdydv8vKaxhKj3QjdlU5A1PS9k"',
+				origin: "https://solscan.io",
+				priority: "u=1, i",
+				referer: "https://solscan.io/",
+				"sol-aut": "5GvLD-NW7B9dls0fKbJHWeJeCUXOPQAbf70dKwfI:",
+				"user-agent": generateRandomUserAgent(),
+			},
+		}
+	);
+
+	return { transactions: formatData(data.data.transactions) };
+};
+
+const getTransaction = async (params, query) => {
+	const { data } = await instance.get(
+		`${endpoint}/transaction/detail?tx=${params.id}`,
+		{
+			headers: {
+				cookie:
+					"__cf_bm=6Nb_BhzZtOdGxVrRrnJoN83pN6rfOtlvuRxjI90nJ0E-1728040658-1.0.1.1-wWYmBJNnH55RvNIFlxo41XFAhHH4kt68pEvyR2fW2Zb_6hVNARhlKxBPCqoZQ_ZhtRQjCeFp6vReppgZbEYgbw; cf_clearance=ibbR6Iox6IDDq4r5WTmuKO0tkKPcwv8GloL.CCp7V3w-1728040665-1.2.1.1-Ef84Taqzk5aSmCiHV321WYtxUv7EIDJTG0SDgp4J.kjIs6gG9J7GVpSV9HE12tuH7xN5vtzdNt1sm6dOee1c20DQO2Lqd.rymeTv.0g340.1jEjK1BnkO4QUlkmEWdbBHzBOc43akGgAef7InJnNEKcg2eO5dPLXT0waBuDCHTs1VMJWBHhfeAE9jZz4C.pPKJKtUDvCyqvR.KRlokRlHo_gnzTVUQDawAtgdLimOOWAK5huvgGQURDoHngS1E5ne03ScjMqfL9HGKME7wXjsK1M9v6zX0WjGMiRDiXkMX3H8oZieb0wyG.j.UKR7jS3K9ElZYtMaZ2kTH1dRJ2DW13.4Q.H3Q.RZlc7Bv3a9FEKf79.2TUxXaiGtKqFH9IbCrPVxFCQ09YxgSp_U0H0AA; _ga=GA1.1.1401977830.1728040664; _ga_PS3V7B7KV0=GS1.1.1728040664.1.1.1728040689.0.0.0; __cf_bm=Mz6cAvTNjPAeUi30bGhjWOHzNal5QLqwS2T6omWu3c8-1728267695-1.0.1.1-oPl.fRzgy_UIvX86I4hXorbEOT5HKcOEAxcTsfbsYnUgtBpIH.Cck88Gm0KcosZyt6sRto_BNTjyJegI2oYryQ",
+				"if-none-match": 'W/"622-Ukdydv8vKaxhKj3QjdlU5A1PS9k"',
+				origin: "https://solscan.io",
+				priority: "u=1, i",
+				referer: "https://solscan.io/",
+				"sol-aut": "5GvLD-NW7B9dls0fKbJHWeJeCUXOPQAbf70dKwfI:",
+				"user-agent": generateRandomUserAgent(),
+			},
+		}
+	);
+
+	return { transaction: data.data };
 };
 
 /**
@@ -100,17 +137,21 @@ const getTransactionsList = async (params, query) => {
  * @param {Object} params
  */
 const getTokens = async (params) => {
-	const { data } = await instance.get(`${endpoint}/account/tokens?address=${params.id}`, {
-		headers: {
-			cookie: "__cf_bm=6Nb_BhzZtOdGxVrRrnJoN83pN6rfOtlvuRxjI90nJ0E-1728040658-1.0.1.1-wWYmBJNnH55RvNIFlxo41XFAhHH4kt68pEvyR2fW2Zb_6hVNARhlKxBPCqoZQ_ZhtRQjCeFp6vReppgZbEYgbw; cf_clearance=ibbR6Iox6IDDq4r5WTmuKO0tkKPcwv8GloL.CCp7V3w-1728040665-1.2.1.1-Ef84Taqzk5aSmCiHV321WYtxUv7EIDJTG0SDgp4J.kjIs6gG9J7GVpSV9HE12tuH7xN5vtzdNt1sm6dOee1c20DQO2Lqd.rymeTv.0g340.1jEjK1BnkO4QUlkmEWdbBHzBOc43akGgAef7InJnNEKcg2eO5dPLXT0waBuDCHTs1VMJWBHhfeAE9jZz4C.pPKJKtUDvCyqvR.KRlokRlHo_gnzTVUQDawAtgdLimOOWAK5huvgGQURDoHngS1E5ne03ScjMqfL9HGKME7wXjsK1M9v6zX0WjGMiRDiXkMX3H8oZieb0wyG.j.UKR7jS3K9ElZYtMaZ2kTH1dRJ2DW13.4Q.H3Q.RZlc7Bv3a9FEKf79.2TUxXaiGtKqFH9IbCrPVxFCQ09YxgSp_U0H0AA; _ga=GA1.1.1401977830.1728040664; _ga_PS3V7B7KV0=GS1.1.1728040664.1.1.1728040689.0.0.0; __cf_bm=Mz6cAvTNjPAeUi30bGhjWOHzNal5QLqwS2T6omWu3c8-1728267695-1.0.1.1-oPl.fRzgy_UIvX86I4hXorbEOT5HKcOEAxcTsfbsYnUgtBpIH.Cck88Gm0KcosZyt6sRto_BNTjyJegI2oYryQ",
-			"if-none-match": 'W/"622-Ukdydv8vKaxhKj3QjdlU5A1PS9k"',
-			origin: "https://solscan.io",
-			priority: "u=1, i",
-			referer: "https://solscan.io/",
-			"sol-aut": "uommLFJFHe0I=7pB9dls0fKSHLSixPco",
-			"user-agent": generateRandomUserAgent(),
-		},
-	});
+	const { data } = await instance.get(
+		`${endpoint}/account/tokens?address=${params.id}`,
+		{
+			headers: {
+				cookie:
+					"__cf_bm=6Nb_BhzZtOdGxVrRrnJoN83pN6rfOtlvuRxjI90nJ0E-1728040658-1.0.1.1-wWYmBJNnH55RvNIFlxo41XFAhHH4kt68pEvyR2fW2Zb_6hVNARhlKxBPCqoZQ_ZhtRQjCeFp6vReppgZbEYgbw; cf_clearance=ibbR6Iox6IDDq4r5WTmuKO0tkKPcwv8GloL.CCp7V3w-1728040665-1.2.1.1-Ef84Taqzk5aSmCiHV321WYtxUv7EIDJTG0SDgp4J.kjIs6gG9J7GVpSV9HE12tuH7xN5vtzdNt1sm6dOee1c20DQO2Lqd.rymeTv.0g340.1jEjK1BnkO4QUlkmEWdbBHzBOc43akGgAef7InJnNEKcg2eO5dPLXT0waBuDCHTs1VMJWBHhfeAE9jZz4C.pPKJKtUDvCyqvR.KRlokRlHo_gnzTVUQDawAtgdLimOOWAK5huvgGQURDoHngS1E5ne03ScjMqfL9HGKME7wXjsK1M9v6zX0WjGMiRDiXkMX3H8oZieb0wyG.j.UKR7jS3K9ElZYtMaZ2kTH1dRJ2DW13.4Q.H3Q.RZlc7Bv3a9FEKf79.2TUxXaiGtKqFH9IbCrPVxFCQ09YxgSp_U0H0AA; _ga=GA1.1.1401977830.1728040664; _ga_PS3V7B7KV0=GS1.1.1728040664.1.1.1728040689.0.0.0; __cf_bm=Mz6cAvTNjPAeUi30bGhjWOHzNal5QLqwS2T6omWu3c8-1728267695-1.0.1.1-oPl.fRzgy_UIvX86I4hXorbEOT5HKcOEAxcTsfbsYnUgtBpIH.Cck88Gm0KcosZyt6sRto_BNTjyJegI2oYryQ",
+				"if-none-match": 'W/"622-Ukdydv8vKaxhKj3QjdlU5A1PS9k"',
+				origin: "https://solscan.io",
+				priority: "u=1, i",
+				referer: "https://solscan.io/",
+				"sol-aut": "uommLFJFHe0I=7pB9dls0fKSHLSixPco",
+				"user-agent": generateRandomUserAgent(),
+			},
+		}
+	);
 
 	const tokenHoldings = {
 		total: data.data.tokens.length,
@@ -130,7 +171,7 @@ const getTokens = async (params) => {
 				name: token.tokenName,
 				amount: token.balance,
 				price: token.priceUsdt,
-				symbol: token.tokensymbol,
+				symbol: token.tokenSymbol,
 				image: token.tokenIcon,
 				address: token.address,
 				tokenAddress: token.tokenAddress,
@@ -148,8 +189,61 @@ const getTokens = async (params) => {
 	return tokenHoldings;
 };
 
+const getTransfers = async (params) => {
+	const { data } = await instance.get(
+		`${endpoint}/account/transfer?address=${params.id}`,
+		{
+			headers: {
+				cookie:
+					"__cf_bm=6Nb_BhzZtOdGxVrRrnJoN83pN6rfOtlvuRxjI90nJ0E-1728040658-1.0.1.1-wWYmBJNnH55RvNIFlxo41XFAhHH4kt68pEvyR2fW2Zb_6hVNARhlKxBPCqoZQ_ZhtRQjCeFp6vReppgZbEYgbw; cf_clearance=ibbR6Iox6IDDq4r5WTmuKO0tkKPcwv8GloL.CCp7V3w-1728040665-1.2.1.1-Ef84Taqzk5aSmCiHV321WYtxUv7EIDJTG0SDgp4J.kjIs6gG9J7GVpSV9HE12tuH7xN5vtzdNt1sm6dOee1c20DQO2Lqd.rymeTv.0g340.1jEjK1BnkO4QUlkmEWdbBHzBOc43akGgAef7InJnNEKcg2eO5dPLXT0waBuDCHTs1VMJWBHhfeAE9jZz4C.pPKJKtUDvCyqvR.KRlokRlHo_gnzTVUQDawAtgdLimOOWAK5huvgGQURDoHngS1E5ne03ScjMqfL9HGKME7wXjsK1M9v6zX0WjGMiRDiXkMX3H8oZieb0wyG.j.UKR7jS3K9ElZYtMaZ2kTH1dRJ2DW13.4Q.H3Q.RZlc7Bv3a9FEKf79.2TUxXaiGtKqFH9IbCrPVxFCQ09YxgSp_U0H0AA; _ga=GA1.1.1401977830.1728040664; _ga_PS3V7B7KV0=GS1.1.1728040664.1.1.1728040689.0.0.0; __cf_bm=Mz6cAvTNjPAeUi30bGhjWOHzNal5QLqwS2T6omWu3c8-1728267695-1.0.1.1-oPl.fRzgy_UIvX86I4hXorbEOT5HKcOEAxcTsfbsYnUgtBpIH.Cck88Gm0KcosZyt6sRto_BNTjyJegI2oYryQ",
+				"if-none-match": 'W/"622-Ukdydv8vKaxhKj3QjdlU5A1PS9k"',
+				origin: "https://solscan.io",
+				priority: "u=1, i",
+				referer: "https://solscan.io/",
+				"sol-aut": "uommLFJFHe0I=7pB9dls0fKSHLSixPco",
+				"user-agent": generateRandomUserAgent(),
+			},
+		}
+	);
+
+	return { transfers: formatDataTranfe(data.data) };
+};
+const getTransfer = async (params) => {
+	return { id: params.id };
+};
+
+function formatData(transactions) {
+	return transactions.map((tx) => ({
+		txHash: tx.txHash,
+		block_time: new Date(tx.blockTime * 1000),
+		block: tx.slot,
+		fee_SOL: tx.fee / 1_000_000_000,
+		status: tx.status,
+		signer_by: tx.signer,
+	}));
+}
+
+function formatDataTranfe(transactions) {
+	return transactions.map((tx) => ({
+		amount: tx.amount,
+		block_id: tx.block_id,
+		value: tx.value.toFixed(9),
+		block_time: new Date(tx.block_time * 1000),
+		transHash: tx.trans_id,
+		from_address: tx.from_address,
+		from_token_account: tx.from_token_account,
+		to_address: tx.to_address,
+		to_token_account: tx.to_token_account,
+		status: tx.status,
+		traffic: tx.flow,
+		signer_by: tx.signer,
+	}));
+}
 module.exports = {
 	getAddress,
-	getTransactionsList,
 	getTokens,
+	getTransactionsList,
+	getTransaction,
+	getTransfers,
+	getTransfer,
 };
