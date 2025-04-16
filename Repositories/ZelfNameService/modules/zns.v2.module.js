1adfc52b8e75a996939e5a54746152321cd9a148
Miguel Treviño <miguel.trevinom@gmail.com>
2025-04-15 19:54:03 -0500

    console log when it fails to create it from offline

--- Repositories/ZelfNameService/modules/zns.v2.module.js
+++ Repositories/ZelfNameService/modules/zns.v2.module.js
@@ -618,6 +618,7 @@ const leaseOffline = async (params, authUser) => {
 	if (!_preview) _preview = await preview({ zelfProof: zelfNameObject.zelfProof });
 
 	if (!zelfName.includes(_preview.publicData.zelfName)) {
+		console.log({ _preview, zelfName, zelfNameObject });
 		const error = new Error("zelfName_does_not_match_in_zelfProof");
 		error.status = 409;
 		throw error;
