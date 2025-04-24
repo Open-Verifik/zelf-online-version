ba87d43db24172fb04c5f2faa1525a4dd89a90f0
anuarmaria <anuarmaria@gmail.com>
2025-04-24 11:49:58 -0500

    eth logo change

--- Repositories/etherscan/modules/etherscan-scrapping.module.js
+++ Repositories/etherscan/modules/etherscan-scrapping.module.js
@@ -78,7 +78,7 @@ const getAddress = async (params) => {
 			name: "Ethereum",
 			price: price,
 			image:
-				"https://nwgz3prwfm5e3gvqyostyhk4avy3ygozgvqlvzd2txqjmwctdzxq.arweave.zelf.world/bY2dvjYrOk2asMOlPB1cBXG8Gdk1YLrkep3gllhTHm8",
+				"https://dynamic-assets.coinbase.com/dbb4b4983bde81309ddab83eb598358eb44375b930b94687ebe38bc22e52c3b2125258ffb8477a5ef22e33d6bd72e32a506c391caa13af64c00e46613c3e5806/asset_icons/4113b082d21cc5fab17fc8f2d19fb996165bcce635e6900f7fc2d57c4ef33ae9.png",
 			amount: data.ETH.balance.toString(),
 		});
 
