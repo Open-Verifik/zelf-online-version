4bcbc4d23133be9a9a88ff148c977ed87b273c68
alisdair-ashbourne <alisdair.ashbourne@gmail.com>
2025-05-27 13:19:42 -0600

    68: Move lifi to node

---
+++ Repositories/Lifi/routes/lifi.route.js
@@ -0,0 +1,20 @@
+const config = require("../../../Core/config.js");
+
+const Controller = require("../controllers/lifi.controller.js");
+const Middleware = require("../middlewares/lifi.middleware.js");
+const SessionMiddleware = require("../../Session/middlewares/session.middleware");
+
+const base = "/lifi";
+
+module.exports = (server) => {
+	const PATH = config.basePath(base);
+
+	server.get(`${PATH}/chains`, SessionMiddleware.validateJWT, Middleware.getChainsValidation, Controller.getChains);
+	server.get(`${PATH}/quote`, SessionMiddleware.validateJWT, Middleware.getQuoteValidation, Controller.getQuote);
+	server.get(`${PATH}/status`, SessionMiddleware.validateJWT, Middleware.getStatusValidation, Controller.getStatus);
+	server.get(`${PATH}/token`, SessionMiddleware.validateJWT, Middleware.getTokenByChainIdValidation, Controller.getTokenByChainId);
+	server.get(`${PATH}/tokens`, SessionMiddleware.validateJWT, Middleware.getTokensValidation, Controller.getTokens);
+	server.get(`${PATH}/tools`, SessionMiddleware.validateJWT, Middleware.getToolsValidation, Controller.getTools);
+
+	server.post(`${PATH}/execute-solana-swap`, SessionMiddleware.validateJWT, Middleware.executeSolanaSwapValidation, Controller.executeSolanaSwap);
+};
