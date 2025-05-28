08b3572db6a780dac7f2e6b8d2f6be11a813af0c
Miguel Treviño <miguel.trevinom@gmail.com>
2025-05-28 09:47:58 -0700

    formatting

--- Repositories/toncoin/routes/toncoin_scrapping.route.js
+++ Repositories/toncoin/routes/toncoin_scrapping.route.js
@@ -1,29 +1,15 @@
 const config = require("../../../Core/config");
 const Controller = require("../controllers/toncoin-scrapping.controller");
-const SessionMiddleware = require("../../Session/middlewares/session.middleware");
 const Middleware = require("../middlewares/toncoin-scrapping.middleware");
 
-const base = "/tonicoin";
+const base = "/ton";
 
 module.exports = (server) => {
 	const PATH = config.basePath(base);
 
-	server.get(
-		`${PATH}/address/:id`,
-		SessionMiddleware.validateJWT,
-		Controller.balance
-	);
+	server.get(`${PATH}/address/:id`, Controller.getAddress);
 
-	server.get(
-		`${PATH}/address/:id/transactions`,
-		SessionMiddleware.validateJWT,
-		Controller.transactionsList
-	);
+	server.get(`${PATH}/address/:id/transactions`, Controller.transactionsList);
 
-	server.get(
-		`${PATH}/transaction/:id`,
-		SessionMiddleware.validateJWT,
-		Middleware.validateToken,
-		Controller.getTransactionDetail
-	);
+	server.get(`${PATH}/transaction/:id`, Middleware.validateToken, Controller.getTransactionDetail);
 };
